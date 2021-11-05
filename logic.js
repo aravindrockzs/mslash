f78d69f0d89f50ddda4be88d97e0a7c665f4149b
aravindrockzs <aravindrockzs@gmail.com>
2021-11-05 23:58:12 +0530

    v2.0 inserts link on click

--- logic.js
+++ logic.js
@@ -3,7 +3,24 @@ const html = document.querySelector('html');
 //div for showing result
 const result = document.createElement('div');
 result.classList.add('mslash');
-console.log(result);
+
+result.addEventListener('click', (e) => {
+  if (e.target.classList.contains('link')) {
+    // const htmlContent = document.querySelector('.editable').innerHTML;
+
+    result.remove();
+    const htmlContent = document.querySelector('.editable').innerHTML;
+
+    document.querySelector('.editable').innerHTML =
+      htmlContent.substring(0, htmlContent.length - 2) +
+      '<a class="link" href="#"> m/ </a>';
+  }
+});
+
+// link
+// const link=`<a href="${desiredLink}">${desiredText}</a>`
+
+result.insertAdjacentHTML('afterbegin', '<a class="link" href="#">link</a>');
 
 const handleTypeEvent = function (el) {
   el.addEventListener('input', () => {
@@ -11,8 +28,6 @@ const handleTypeEvent = function (el) {
       console.log('working');
 
       el.appendChild(result);
-    } else {
-      el.removeChild(result);
     }
     el.removeEventListener('input', () => {});
   });
